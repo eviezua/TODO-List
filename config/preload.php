73205013783e252d772c9eb515642d168676ea1a
evie <eviezua@gmail.com>
2023-12-01 19:49:24 +0200

    Setting up sf project in docker compose.

---
+++ config/preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php-fpm')) {
+    require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php-fpm';
+}
