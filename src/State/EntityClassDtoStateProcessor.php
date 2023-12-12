61354d8db1669407044bf54b5bc2a84ccdc4863a
evie <eviezua@gmail.com>
2023-12-12 17:45:37 +0200

    Add processor and provider for API

---
+++ src/State/EntityClassDtoStateProcessor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Doctrine\Common\State\RemoveProcessor;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\DeleteOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+class EntityClassDtoStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        #[Autowire(service: PersistProcessor::class)] private ProcessorInterface $persistProcessor,
+        #[Autowire(service: RemoveProcessor::class)] private ProcessorInterface $removeProcessor,
+        private UserPasswordHasherInterface $userPasswordHasher,
+        private MicroMapperInterface $microMapper,
+    ) {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        $stateOptions = $operation->getStateOptions();
+        assert($stateOptions instanceof Options);
+        $entityClass = $stateOptions->getEntityClass();
+
+        $entity = $this->mapDtoToEntity($data, $entityClass);
+
+        if ($operation instanceof DeleteOperationInterface) {
+            $this->removeProcessor->process($entity, $operation, $uriVariables, $context);
+            return null;
+        }
+
+        $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+
+        $data->id = $entity->getId();
+
+        return $data;
+    }
+
+    private function mapDtoToEntity(object $dto, string $entityClass): object
+    {
+        return $this->microMapper->map($dto, $entityClass);
+    }
+}
