d435f85d6d5d4d88b76519c5443b5b78cd1578a6
evie <eviezua@gmail.com>
2023-12-13 21:32:41 +0200

    Fix migrations

--- migrations/Version20231206152645.php
+++ migrations/Version20231206152645.php
@@ -20,25 +20,21 @@ final class Version20231206152645 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE task_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE task (id INT NOT NULL, owner_id INT NOT NULL, status VARCHAR(255) NOT NULL, priority INT NOT NULL, title VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, completed_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE task ADD owner_id INT NOT NULL');
         $this->addSql('CREATE INDEX IDX_527EDB257E3C61F9 ON task (owner_id)');
         $this->addSql('COMMENT ON COLUMN task.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN task.completed_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
-        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB257E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql(
+            'ALTER TABLE task ADD CONSTRAINT FK_527EDB257E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE'
+        );
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE task_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('DROP INDEX IDX_527EDB257E3C61F9');
         $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB257E3C61F9');
-        $this->addSql('DROP TABLE task');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('ALTER TABLE task DROP owner_id');
     }
 }
